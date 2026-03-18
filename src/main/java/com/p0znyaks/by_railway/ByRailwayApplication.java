b672661329e056e86126555eca3800a7d5ff8ddd
p0znyaks <p0zny4ks@yandex.ru>
2026-03-18 15:22:48 +0300

    feat: initialize Spring Boot project

---
+++ src/main/java/com/p0znyaks/by_railway/ByRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.p0znyaks.by_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p0znyaks/by_railway/ByRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p0znyaks.by_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
